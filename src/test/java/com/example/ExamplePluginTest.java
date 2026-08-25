35f68afbb52aad31ce6e8b5775815e8141500deb
StackBot-Team <225417993+Tannhausr-Gate@users.noreply.github.com>
2026-08-25 16:32:12 -0400

    checking

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -7,7 +7,7 @@ public class ExamplePluginTest
 {
     public static void main(String[] args) throws Exception
     {
-        ExternalPluginManager.loadBuiltin(com.example.runeconnect.RuneBotConnectPlugin.class);
+        ExternalPluginManager.loadBuiltin(com.example.runeconnect.RuneConnectPlugin.class);
         RuneLite.main(args);
     }
 }
